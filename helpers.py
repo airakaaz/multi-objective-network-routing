ce96bc7d9f7762ea5d63e0a3885d78fc352af467
kaz <kizo.aria@protonmail.com>
2025-12-29 23:24:36 +0100

    implement path_stats function to visualize algorithm's results
    path_stats:
    - print the shortest path
    - print and return total latency and risk
    
    minor:
    - add pycache to gitignore (correctly this time)

--- helpers.py
+++ helpers.py
@@ -1,2 +1,21 @@
 def combine(lat, risk, a=1, b=10):
     return a * lat + b * risk
+
+
+def path_stats(graph, path):
+    lat = risk = 0
+    curr = path[0]
+    print(curr, end=" ")
+
+    for hop in path[1:]:
+        print("->", hop, end=" ")
+        edge = graph.edges[(curr, hop)]
+        lat += edge[0]
+        risk += edge[1]
+        curr = hop
+
+    print()
+    print("total latency:", lat)
+    print("total risk", risk)
+
+    return (lat, risk)
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from model import Graph
-from helpers import combine
+from helpers import combine, path_stats
+from algorithms import dijkstra
 
 if __name__ == "__main__":
     graph = Graph(combine, path="data.csv")
+    path = dijkstra(graph, combine)
+    path_stats(graph, path)
     graph.visualize()
